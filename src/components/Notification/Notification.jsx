ed53fbbb7449da9fe9526228c0eb53193cd5991d
Pavlo-Savka <savka.pavlo@gmail.com>
2023-04-22 02:20:23 +0300

    HW2 task1

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'; 
+import React, { Component } from 'react';
+
+class Notification extends Component { 
+    render() {
+        return (
+            <h2>
+                {this.props.message}
+            </h2>
+        )
+    }
+}
+export default Notification;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'; 
+import React, { Component } from 'react';
+// import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+// import Statistics from './Statistics/Statistics';
+
+class Section extends Component {
+    render() {
+        return (
+            <h2>{this.props.title}</h2>  
+  )
+} ;
+}
+export default Section;
